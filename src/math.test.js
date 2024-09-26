1a1daecbe477e3578e81e94111c909bfcab014d8
Fatima Yarhamidi <fatima.yarhamidi@gmail.com>
2024-09-26 14:47:14 +1200

    first commit

---
+++ src/math.test.js
@@ -0,0 +1,8 @@
+import { describe, it, expect } from 'vitest'
+import { add } from './math'
+
+describe('add', () => {
+  it('should return correct sum', () => {
+    expect(add(2, 3)).toBe(5)
+  })
+})
